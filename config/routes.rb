6cecf8f996c7496642e437e8fbd85b426acd2311
Heba Dorazahi <hebamirdora@gmail.com>
2025-12-28 19:47:22 +0100

    Ruby on rails beginner

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   #get "home/index"
   get "home/about"
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_28_102055) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_28_182308) do
   create_table "friends", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -20,5 +20,19 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_28_102055) do
     t.string "string"
     t.string "twitter"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
